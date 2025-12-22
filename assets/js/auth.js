160440dcacfb4c0f8eb2388aa5f6d1b8c4359b6d
Shadow04466 <124391371+Shadow04466@users.noreply.github.com>
2025-12-22 23:48:56 +0500

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,26 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword, onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+const msg = document.getElementById("msg");
+
+loginBtn?.addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => {
+      msg.innerText = err.message;
+    });
+});
+
+// 🔐 Protect dashboard
+onAuthStateChanged(auth, user => {
+  if (!user && location.pathname.includes("dashboard")) {
+    window.location.href = "login.html";
+  }
+});
